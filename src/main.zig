58d9f4a1a6237ae33657a03d01278560c9a4efbc
smallbraintime <patrykstruzek@proton.me>
2025-04-06 00:25:29 +0200

    clean and segment the code, minor changes

--- src/main.zig
+++ src/main.zig
@@ -1,7 +1,13 @@
 const std = @import("std");
 const at = @import("asciitecture");
 
-const bird =
+const WIDTH = 105;
+const HEIGHT = 35;
+const SCREEN_BOTTOM = HEIGHT / 2;
+const SCREEN_RIGHT = WIDTH / 2;
+const VELOCITYX = 5.0;
+const VELOCITYY = 15.0;
+const BIRD =
     \\╭───╮
     \\│  ^│>
     \\╰─V─╯
@@ -16,24 +22,122 @@ const PipeSegment = struct {
         var rn: [1]u8 = undefined;
         std.posix.getrandom(&rn) catch unreachable;
         var rng = std.rand.DefaultPrng.init(@intCast(rn[0]));
-        const yoffset: f32 = @floatFromInt(rng.random().intRangeAtMost(u8, 2, 10));
-        const y = 35 / yoffset;
 
-        var pipe_segment: PipeSegment = undefined;
+        const divider: f32 = @floatFromInt(rng.random().intRangeAtMost(u8, 2, 10));
+        const yoffset = 35 / divider;
 
-        pipe_segment.bottom = at.math.Rectangle.init(
-            at.math.vec2(xpos, screen_bottom - y),
+        const bottom = at.math.Rectangle.init(
+            at.math.vec2(xpos, screen_bottom - yoffset),
             @floatFromInt(5),
             @floatFromInt(20),
         );
 
-        pipe_segment.top = at.math.Rectangle.init(
-            pipe_segment.bottom.pos.sub(&at.math.vec2(0, 15 + 20)),
+        const top = at.math.Rectangle.init(
+            bottom.pos.sub(&at.math.vec2(0, 15 + 20)),
             @floatFromInt(5),
             @floatFromInt(20),
         );
 
-        return pipe_segment;
+        return .{
+            .bottom = bottom,
+            .top = top,
+        };
+    }
+};
+
+pub const GameState = struct {
+    bird_collider: at.math.Rectangle,
+    pipe_segments: std.ArrayListUnmanaged(PipeSegment),
+    jumping: bool = false,
+    result: usize = 0,
+    lost: bool = false,
+    time_elapsed: f32 = 0,
+
+    pub fn init(
+        allocator: std.mem.Allocator,
+        bird_width: f32,
+        bird_height: f32,
+    ) !GameState {
+        const bird_collider = at.math.Rectangle.init(
+            at.math.vec2(0, 0),
+            bird_width,
+            bird_height,
+        );
+
+        var pipe_segments = try std.ArrayListUnmanaged(PipeSegment).initCapacity(
+            allocator,
+            20,
+        );
+        errdefer pipe_segments.deinit(allocator);
+        for (0..20) |i| {
+            pipe_segments.appendAssumeCapacity(PipeSegment.init(
+                SCREEN_RIGHT + @as(f32, @floatFromInt(i)) * 20,
+                SCREEN_BOTTOM,
+            ));
+        }
+
+        return .{
+            .bird_collider = bird_collider,
+            .pipe_segments = pipe_segments,
+        };
+    }
+
+    pub fn deinit(self: *GameState, allocator: std.mem.Allocator) void {
+        self.pipe_segments.deinit(allocator);
+    }
+
+    pub fn update(
+        self: *GameState,
+        allocator: std.mem.Allocator,
+        input: *at.input.Input,
+        delta_time: f32,
+    ) !void {
+        if (input.contains(.space) and !self.jumping) self.jumping = true;
+
+        if (self.jumping) {
+            self.bird_collider.pos = self.bird_collider.pos.sub(&at.math.vec2(
+                0,
+                VELOCITYY * 2 * delta_time,
+            ));
+            self.time_elapsed += delta_time;
+
+            if (self.time_elapsed >= 0.5) {
+                self.jumping = false;
+                self.time_elapsed = 0;
+            }
+        }
+
+        self.bird_collider.pos = self.bird_collider.pos.add(&at.math.vec2(
+            0,
+            VELOCITYY * delta_time,
+        ));
+
+        for (self.pipe_segments.items) |*item| {
+            item.top.pos = item.top.pos.sub(&at.math.vec2(VELOCITYX * delta_time, 0));
+            item.bottom.pos = item.bottom.pos.sub(&at.math.vec2(VELOCITYX * delta_time, 0));
+
+            if (self.bird_collider.collidesWith(&.{ .rectangle = item.top }) or
+                self.bird_collider.collidesWith(&.{ .rectangle = item.bottom }))
+            {
+                self.lost = true;
+            }
+
+            if (item.top.pos.x() < 0 and !item.visited) {
+                item.visited = true;
+                self.result += 1;
+            }
+        }
+
+        if (self.pipe_segments.items[0].top.pos.x() < -SCREEN_RIGHT) {
+            _ = self.pipe_segments.orderedRemove(0);
+        }
+
+        if (self.pipe_segments.getLast().top.pos.x() < SCREEN_RIGHT) {
+            try self.pipe_segments.append(allocator, PipeSegment.init(
+                self.pipe_segments.getLast().top.pos.x() + 20,
+                SCREEN_BOTTOM,
+            ));
+        }
     }
 };
 
@@ -41,7 +145,11 @@ pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     defer if (gpa.deinit() == .leak) @panic("memory leak occured");
 
-    var term = try at.Terminal(at.LinuxTty).init(gpa.allocator(), 60, .{ .width = 105, .height = 35 });
+    var term = try at.Terminal(at.LinuxTty).init(
+        gpa.allocator(),
+        60,
+        .{ .width = 105, .height = 35 },
+    );
     defer term.deinit() catch unreachable;
     term.setBg(at.style.IndexedColor.bright_blue);
 
@@ -50,86 +158,81 @@ pub fn main() !void {
     var input = try at.input.Input.init();
     defer input.deinit() catch unreachable;
 
-    const bird_sprite = at.sprite.Sprite.init(bird, .{ .fg = at.style.IndexedColor.yellow, .bg = at.style.IndexedColor.bright_magenta });
+    const bird_sprite = at.sprite.Sprite.init(
+        BIRD,
+        .{ .fg = at.style.IndexedColor.yellow },
+    );
 
-    var bird_pos: at.math.Vec2 = at.math.vec2(0, 0);
-    const velocityx: f32 = 10.0;
-    const velocityy: f32 = 15.0;
-    var jumping = false;
-    var counter: f32 = 0;
-    var pipesx: f32 = 0;
+    const paragraph_config = at.widgets.Paragraph.ParagraphConfig{
+        .border_style = .{
+            .border = .rounded,
+            .style = .{
+                .fg = at.style.IndexedColor.white,
+                .attr = .bold,
+            },
+        },
+        .text_style = .{
+            .fg = at.style.IndexedColor.white,
+            .attr = .bold,
+        },
+        .filling = true,
+        .animation = .{
+            .speed = 5,
+            .looping = true,
+        },
+    };
+    var game_over_popup = try at.widgets.Paragraph.init(
+        gpa.allocator(),
+        &[_][]const u8{ "GAME OVER", " [space] " },
+        paragraph_config,
+    );
+    defer game_over_popup.deinit();
+
     const bird_dims = try bird_sprite.dims();
-    var bird_collider = at.math.Rectangle.init(at.math.vec2(0, 0), @floatFromInt(bird_dims.width), @floatFromInt(bird_dims.height));
-    var result: usize = 0;
 
-    var pipes = try std.ArrayList(PipeSegment).initCapacity(gpa.allocator(), term._win_size.width / 20);
-    defer pipes.deinit();
-    const screen_bottom = 35 / 2;
-    const screen_right = 105 / 2;
-    for (0..20) |i| {
-        try pipes.append(PipeSegment.init(
-            screen_right + @as(f32, @floatFromInt(i)) * 20,
-            screen_bottom,
-        ));
-    }
-    // blk:
-    while (true) {
-        if (input.contains(.escape)) break;
-        if (input.contains(.space) and !jumping) jumping = true;
+    blk: while (true) {
+        var game_state = try GameState.init(
+            gpa.allocator(),
+            @floatFromInt(bird_dims.width),
+            @floatFromInt(bird_dims.height),
+        );
+        defer game_state.deinit(gpa.allocator());
 
-        if (jumping) {
-            bird_pos = bird_pos.sub(&at.math.vec2(0, velocityy * 2 * term.delta_time));
-            counter += term.delta_time;
-            if (counter >= 0.5) {
-                jumping = false;
-                counter = 0;
+        while (true) {
+            if (input.contains(.escape)) break :blk;
+
+            if (!game_state.lost) {
+                try game_state.update(gpa.allocator(), &input, term.delta_time);
             }
-        }
 
-        bird_pos = bird_pos.add(&at.math.vec2(0, velocityy * term.delta_time));
-        bird_collider.pos = bird_pos;
-        pipesx -= velocityx * term.delta_time;
-
-        for (pipes.items) |*item| {
-            item.top.pos = item.top.pos.sub(&at.math.vec2(5 * term.delta_time, 0));
-            item.bottom.pos = item.bottom.pos.sub(&at.math.vec2(5 * term.delta_time, 0));
             painter.setCell(&.{ .bg = at.style.IndexedColor.green });
-            painter.drawRectangleShape(&item.bottom, false);
-            painter.drawRectangleShape(&item.top, false);
-            if (bird_collider.collidesWith(&.{ .rectangle = item.top }) or bird_collider.collidesWith(&.{ .rectangle = item.bottom })) {
-                try painter.drawText("GAME OVER", &at.math.vec2(0, 0));
-                // try term.draw();
-                // while (true) {
-                //     if (input.contains(.space)) break :blk;
-                // }
+            for (game_state.pipe_segments.items) |*item| {
+                painter.drawRectangleShape(&item.bottom, true);
+                painter.drawRectangleShape(&item.top, true);
             }
 
-            if (item.top.pos.x() < 0 and !item.visited) {
-                item.visited = true;
-                result += 1;
+            try bird_sprite.draw(&painter, &game_state.bird_collider.pos);
+
+            painter.setDrawingSpace(.screen);
+            painter.setCell(&.{
+                .fg = at.style.IndexedColor.white,
+                .bg = at.style.IndexedColor.black,
+            });
+            var buf: [5]u8 = undefined;
+            const fps = try std.fmt.bufPrint(&buf, "{}", .{game_state.result});
+            try painter.drawText(fps, &at.math.vec2(0, -SCREEN_BOTTOM));
+            painter.setDrawingSpace(.world);
+
+            if (game_state.lost) {
+                try game_over_popup.draw(
+                    &painter,
+                    &at.math.vec2(0, 0),
+                    term.delta_time,
+                );
+                if (input.contains(.space)) break;
             }
+
+            try term.draw();
         }
-
-        if (pipes.items[0].top.pos.x() < -screen_right) {
-            _ = pipes.orderedRemove(0);
-        }
-
-        if (pipes.getLast().top.pos.x() < screen_right) {
-            try pipes.append(PipeSegment.init(
-                pipes.getLast().top.pos.x() + 20,
-                screen_bottom,
-            ));
-        }
-
-        try bird_sprite.draw(&painter, &bird_pos);
-
-        painter.setDrawingSpace(.screen);
-        painter.setCell(&.{ .fg = at.style.IndexedColor.white, .bg = at.style.IndexedColor.black });
-        var buf: [5]u8 = undefined;
-        const fps = try std.fmt.bufPrint(&buf, "{}", .{result});
-        try painter.drawText(fps, &(at.math.vec2((105 / 2) - 4, -35 / 2 - 0.5)));
-        painter.setDrawingSpace(.world);
-
-        try term.draw();
     }
 }
